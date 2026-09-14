e98dc37238ccfe41e169ed9b13ea718818a84adf
Fel-lek <dobrzyckip0@gmail.com>
2026-09-14 19:52:41 +0100

    Education And Stuffs

--- taskbar/taskbar.js
+++ taskbar/taskbar.js
@@ -1,17 +1,19 @@
-fetch("taskbar/taskbar.html")
+fetch("../taskbar/taskbar.html")
     .then(response => response.text())
     .then(html => {
         document.getElementById("taskbar").innerHTML = html;
 
         const css = document.createElement("link");
         css.rel = "stylesheet";
-        css.href = "taskbar/taskbar.css";
+        css.href = "../taskbar/taskbar.css";
         document.head.appendChild(css);
 
-        const currentPage = window.location.pathname.split("/").pop() || "index.html";
+        const currentPage = window.location.pathname.split("/").pop();
 
         document.querySelectorAll(".taskbar a").forEach(link => {
-            if (link.getAttribute("href") === currentPage) {
+            const linkPage = link.getAttribute("href").split("/").pop();
+
+            if (linkPage === currentPage) {
                 link.classList.add("active");
             }
         });
